eeda7df63fcfad4aacada11e21211209d3cc80b0
Ben W <bwiedeback@gmail.com>
2020-11-03 19:29:19 -0700

    keypr

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 dotnet publish -c Release
-docker build -t keypr ./bin/Release/netcoreapp3.1/publish
-docker tag keypr registry.heroku.com/keypr/web
+docker build -t "keypr" ./bin/Release/netcoreapp3.1/publish
+docker tag "keypr" registry.heroku.com/keypr/web
 docker push registry.heroku.com/keypr/web
 heroku container:release web -a keypr
 echo press any key
