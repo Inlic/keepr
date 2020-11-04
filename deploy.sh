cd18a42745d3bf20b9c119bc6ece04c70a23bac5
Ben W <bwiedeback@gmail.com>
2020-11-03 18:22:09 -0700

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 dotnet publish -c Release
-docker build -t TAG_NAME ./bin/Release/netcoreapp3.1/publish
-docker tag TAG_NAME registry.heroku.com/HEROKU_APP_NAME/web
-docker push registry.heroku.com/HEROKU_APP_NAME/web
-heroku container:release web -a HEROKU_APP_NAME
+docker build -t keypr ./bin/Release/netcoreapp3.1/publish
+docker tag keypr registry.heroku.com/keypr/web
+docker push registry.heroku.com/keypr/web
+heroku container:release web -a keypr
+echo press any key
+read end
